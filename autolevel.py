312159d22c3be2742c7821150ab169801da20ab6
wfsec <69239087+wfsecs@users.noreply.github.com>
2024-03-17 17:37:02 +0200

    Add files via upload

---
+++ autolevel.py
@@ -0,0 +1,41 @@
+from colorama import Fore, init
+import requests
+import random
+import time
+
+init()
+
+token = input('Your token: ')
+channel_id = input('Channel ID: ')
+messages = ['i agree', 'true', 'real', 'fax dude', 'yeah', 'exactly', 'true af', 'definitely', 'definitely true', 'fax', 'indeed']
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+    'Authorization': token
+}
+
+print(f'''
+{Fore.CYAN}
+
+             $$$$$$\              $$\               $$\                               $$\ 
+            $$  __$$\             $$ |              $$ |                              $$ |
+            $$ /  $$ |$$\   $$\ $$$$$$\    $$$$$$\  $$ | $$$$$$\ $$\    $$\  $$$$$$\  $$ |
+            $$$$$$$$ |$$ |  $$ |\_$$  _|  $$  __$$\ $$ |$$  __$$\\$$\  $$  |$$  __$$\ $$ |
+            $$  __$$ |$$ |  $$ |  $$ |    $$ /  $$ |$$ |$$$$$$$$ |\$$\$$  / $$$$$$$$ |$$ |
+            $$ |  $$ |$$ |  $$ |  $$ |$$\ $$ |  $$ |$$ |$$   ____| \$$$  /  $$   ____|$$ |
+            $$ |  $$ |\$$$$$$  |  \$$$$  |\$$$$$$  |$$ |\$$$$$$$\   \$  /   \$$$$$$$\ $$ |
+            \__|  \__| \______/    \____/  \______/ \__| \_______|   \_/     \_______|\__|                                                                                                                       
+{Fore.RESET}       
+    ---------------------------------------------------------------------------------------------                                                                    
+                                                                                    
+''')
+
+while True:
+    wait_time = random.randint(70, 200)
+
+    message = random.choice(messages)
+    json_data = {
+        'content': message
+    }
+    r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=headers, json=json_data)
+    print(f'            {Fore.YELLOW}[%] {Fore.YELLOW}[{r.status_code}] {Fore.LIGHTBLACK_EX}[Waiting {str(wait_time)} seconds or {str(wait_time/60)[0:4]} minutes...] {Fore.RESET} Sent message > {message}')
+    time.sleep(wait_time)
